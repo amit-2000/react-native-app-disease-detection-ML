fb169e3f32efcf1c2104f6e22cdee7d8260bf0a8
amit <amitperane8473@gmail.com>
2023-02-07 22:44:54 +0530

    Feature: upload through camera

--- App.js
+++ App.js
@@ -1,275 +1,128 @@
-import React, { useState } from "react";
+import React from "react";
+
 import {
-  SafeAreaView,
-  Image,
-  StatusBar,
   StyleSheet,
   Text,
-  Platform,
-  Dimensions,
-  useColorScheme,
   View,
   TouchableOpacity,
-  ImageBackground,
+  Button,
+  Image,
 } from "react-native";
-import axios from "axios";
-import Config from "react-native-config";
-import { launchCamera, launchImageLibrary } from "react-native-image-picker";
-import { Colors } from "react-native/Libraries/NewAppScreen";
-import PermissionsService, { isIOS } from "./Permissions";
 
-axios.interceptors.request.use(
-  async (config) => {
-    let request = config;
-    request.headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
+import ImagePicker from "react-native-image-picker";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      resourcePath: {},
     };
-    request.url = configureUrl(config.url);
-    return request;
-  },
-  (error) => error
-);
-
-export const { height, width } = Dimensions.get("window");
-
-export const configureUrl = (url) => {
-  let authUrl = url;
-  if (url && url[url.length - 1] === "/") {
-    authUrl = url.substring(0, url.length - 1);
   }
-  return authUrl;
-};
 
-export const fonts = {
-  Bold: { fontFamily: "Roboto-Bold" },
-};
+  selectFile = () => {
+    var options = {
+      title: "Select Image",
 
-const options = {
-  mediaType: "photo",
-  quality: 1,
-  width: 256,
-  height: 256,
-  includeBase64: true,
-};
+      customButtons: [
+        {
+          name: "customOptionKey",
 
-const App = () => {
-  const [result, setResult] = useState("");
-  const [label, setLabel] = useState("");
-  const isDarkMode = useColorScheme() === "dark";
-  const [image, setImage] = useState("");
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+          title: "Choose file from Custom Option",
+        },
+      ],
 
-  const getPredication = async (params) => {
-    return new Promise((resolve, reject) => {
-      var bodyFormData = new FormData();
-      bodyFormData.append("file", params);
-      const url = Config.URL;
-      return axios
-        .post(url, bodyFormData)
-        .then((response) => {
-          resolve(response);
-        })
-        .catch((error) => {
-          setLabel("Failed to predicting.");
-          reject("err", error);
-        });
-    });
-  };
+      storageOptions: {
+        skipBackup: true,
 
-  const manageCamera = async (type) => {
-    try {
-      if (!(await PermissionsService.hasCameraPermission())) {
-        return [];
-      } else {
-        if (type === "Camera") {
-          openCamera();
-        } else {
-          openLibrary();
-        }
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const openCamera = async () => {
-    launchCamera(options, async (response) => {
-      if (response.didCancel) {
-        console.log("User cancelled image picker");
-      } else if (response.error) {
-        console.log("ImagePicker Error: ", response.error);
-      } else if (response.customButton) {
-        console.log("User tapped custom button: ", response.customButton);
-      } else {
-        const uri = response?.assets[0]?.uri;
-        const path = Platform.OS !== "ios" ? uri : "file://" + uri;
-        getResult(path, response);
-      }
-    });
-  };
-
-  const clearOutput = () => {
-    setResult("");
-    setImage("");
-  };
-
-  const getResult = async (path, response) => {
-    setImage(path);
-    setLabel("Predicting...");
-    setResult("");
-    const params = {
-      uri: path,
-      name: response.assets[0].fileName,
-      type: response.assets[0].type,
+        path: "images",
+      },
     };
-    const res = await getPredication(params);
-    if (res?.data?.class) {
-      setLabel(res.data.class);
-      setResult(res.data.confidence);
-    } else {
-      setLabel("Failed to predict");
-    }
-  };
 
-  const openLibrary = async () => {
-    launchImageLibrary(options, async (response) => {
-      if (response.didCancel) {
+    ImagePicker.showImagePicker(options, (res) => {
+      console.log("Response = ", res);
+
+      if (res.didCancel) {
         console.log("User cancelled image picker");
-      } else if (response.error) {
-        console.log("ImagePicker Error: ", response.error);
-      } else if (response.customButton) {
-        console.log("User tapped custom button: ", response.customButton);
+      } else if (res.error) {
+        console.log("ImagePicker Error: ", res.error);
+      } else if (res.customButton) {
+        console.log("User tapped custom button: ", res.customButton);
+
+        alert(res.customButton);
       } else {
-        const uri = response.assets[0].uri;
-        const path = Platform.OS !== "ios" ? uri : "file://" + uri;
-        getResult(path, response);
+        let source = res;
+
+        this.setState({
+          resourcePath: source,
+        });
       }
     });
   };
 
-  return (
-    <View style={[backgroundStyle, styles.outer]}>
-      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
-      <ImageBackground
-        blurRadius={10}
-        source={{ uri: "background" }}
-        style={{ height: height, width: width }}
-      />
-      <Text style={styles.title}>{"Potato Disease \nPrediction App"}</Text>
-      <TouchableOpacity onPress={clearOutput} style={styles.clearStyle}>
-        <Image source={{ uri: "clean" }} style={styles.clearImage} />
-      </TouchableOpacity>
-      {(image?.length && (
-        <Image source={{ uri: image }} style={styles.imageStyle} />
-      )) ||
-        null}
-      {(result && label && (
-        <View style={styles.mainOuter}>
-          <Text style={[styles.space, styles.labelText]}>
-            {"Label: \n"}
-            <Text style={styles.resultText}>{label}</Text>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.container}>
+          <Image
+            source={{
+              uri: "data:image/jpeg;base64," + this.state.resourcePath.data,
+            }}
+            style={{ width: 100, height: 100 }}
+          />
+
+          <Image
+            source={{ uri: this.state.resourcePath.uri }}
+            style={{ width: 200, height: 200 }}
+          />
+
+          <Text style={{ alignItems: "center" }}>
+            {this.state.resourcePath.uri}
           </Text>
-          <Text style={[styles.space, styles.labelText]}>
-            {"Confidence: \n"}
-            <Text style={styles.resultText}>
-              {parseFloat(result).toFixed(2) + "%"}
-            </Text>
-          </Text>
+
+          <TouchableOpacity onPress={this.selectFile} style={styles.button}>
+            <Text style={styles.buttonText}>Select File</Text>
+          </TouchableOpacity>
         </View>
-      )) ||
-        (image && <Text style={styles.emptyText}>{label}</Text>) || (
-          <Text style={styles.emptyText}>
-            Use below buttons to select a picture of a potato plant leaf.
-          </Text>
-        )}
-      <View style={styles.btn}>
-        <TouchableOpacity
-          activeOpacity={0.9}
-          onPress={() => manageCamera("Camera")}
-          style={styles.btnStyle}
-        >
-          <Image source={{ uri: "camera" }} style={styles.imageIcon} />
-        </TouchableOpacity>
-        <TouchableOpacity
-          activeOpacity={0.9}
-          onPress={() => manageCamera("Photo")}
-          style={styles.btnStyle}
-        >
-          <Image source={{ uri: "gallery" }} style={styles.imageIcon} />
-        </TouchableOpacity>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  title: {
-    alignSelf: "center",
-    position: "absolute",
-    top: (isIOS && 35) || 10,
-    fontSize: 30,
-    ...fonts.Bold,
-    color: "#FFF",
-  },
-  clearImage: { height: 40, width: 40, tintColor: "#FFF" },
-  mainOuter: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    position: "absolute",
-    top: height / 1.6,
-    alignSelf: "center",
-  },
-  outer: {
+  container: {
     flex: 1,
+
+    padding: 30,
+
     alignItems: "center",
+
     justifyContent: "center",
+
+    backgroundColor: "#fff",
   },
-  btn: {
-    position: "absolute",
-    bottom: 40,
-    justifyContent: "space-between",
-    flexDirection: "row",
+
+  button: {
+    width: 250,
+
+    height: 60,
+
+    backgroundColor: "#3740ff",
+
+    alignItems: "center",
+
+    justifyContent: "center",
+
+    borderRadius: 4,
+
+    marginBottom: 12,
   },
-  btnStyle: {
-    backgroundColor: "#FFF",
-    opacity: 0.8,
-    marginHorizontal: 30,
-    padding: 20,
-    borderRadius: 20,
-  },
-  imageStyle: {
-    marginBottom: 50,
-    width: width / 1.5,
-    height: width / 1.5,
-    borderRadius: 20,
-    position: "absolute",
-    borderWidth: 0.3,
-    borderColor: "#FFF",
-    top: height / 4.5,
-  },
-  clearStyle: {
-    position: "absolute",
-    top: 100,
-    right: 30,
-    tintColor: "#FFF",
-    zIndex: 10,
-  },
-  space: { marginVertical: 10, marginHorizontal: 10 },
-  labelText: { color: "#FFF", fontSize: 20, ...fonts.Bold },
-  resultText: { fontSize: 32, ...fonts.Bold },
-  imageIcon: { height: 40, width: 40, tintColor: "#000" },
-  emptyText: {
-    position: "absolute",
-    top: height / 1.6,
-    alignSelf: "center",
-    color: "#FFF",
-    fontSize: 20,
-    maxWidth: "70%",
-    ...fonts.Bold,
+
+  buttonText: {
+    textAlign: "center",
+
+    fontSize: 15,
+
+    color: "#fff",
   },
 });
-
-export default App;
